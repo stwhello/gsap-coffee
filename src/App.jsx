9d7303ca00b9d5960542aa3025aabd84c6452b74
stwhello <sherinpaul2001@gmail.com>
2025-12-29 18:04:27 +0530

    hero animation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import gsap from "gsap";
 import Navbar from "./components/Navbar.jsx";
+import HeroSection from "./sections/HeroSection.jsx";
+import  { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
         <main>
             <Navbar />
+            <HeroSection />
         </main>
     )
 }
